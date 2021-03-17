3c705815da978efcf6d168a46521a91b4f8d581b
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2021-03-17 11:08:30 +0000

    Nos falta el profesor

--- src/ejercicio-clase-2/ejercicio-clase-2.ts
+++ src/ejercicio-clase-2/ejercicio-clase-2.ts
@@ -2,11 +2,18 @@ import {Persona} from '../ejercicio-clase-1/ejercicio-clase-1';
 
 export class Estudiante extends Persona {
   private correo_electronico: string;
-  private asignatura: string;
 
-  constructor(nombre: string, apellido: string, fecha_nacimiento: number, genero: string, correo_electronico: string, asignatura: string) {
+  constructor(nombre: string, apellido: string, fecha_nacimiento: number, genero: string, correo_electronico: string) {
     super(nombre, apellido, fecha_nacimiento, genero);
     this.correo_electronico = correo_electronico;
-    this.asignatura;
+  }
+
+  get_correo_electronico() {
+    return this.correo_electronico;
+  }
+
+  set_correo_electronico(correo_electronico: string) {
+    this.correo_electronico = correo_electronico;
+    return this.correo_electronico;
   }
 }
--- test/ejercicio-clase-2.spec.ts
+++ test/ejercicio-clase-2.spec.ts
@@ -5,7 +5,7 @@ import {Estudiante} from '../src/ejercicio-clase-2/ejercicio-clase-2';
 describe('Test que accede a los atributos de la clase estudiante', () => {
   it('Se accede al nombre, apellido, fecha de nacimiento, genero, correo electronico, asignatura', () => {
     // una variable con mi nombre, para la prueba
-    let estudiante_1: Estudiante = new Estudiante('yago', 'perez', 2000, 'hombre', 'yagoy90@gmail.com', 'dsi');
+    let estudiante_1: Estudiante = new Estudiante('yago', 'perez', 2000, 'hombre', 'yagoy90@gmail.com');
 
     expect(estudiante_1.get_nombre()).to.be.equal('yago');
     expect(estudiante_1.get_apellido()).to.be.equal('perez');
@@ -13,7 +13,18 @@ describe('Test que accede a los atributos de la clase estudiante', () => {
     expect(estudiante_1.get_fecha_nacimiento()).not.to.be.equal(1999);
     expect(estudiante_1.get_genero()).to.be.equal('hombre');
     expect(estudiante_1.get_correo_electronico()).to.be.equal('yagoy90@gmail.com');
-    expect(estudiante_1.get_correo_asignatura()).to.be.equal('dsi');
+  });
+
+  it('Se accede al nombre, apellido, fecha de nacimiento, genero, correo electronico, asignatura', () => {
+    // una variable con mi nombre, para la prueba
+    let estudiante_1: Estudiante = new Estudiante('yago', 'perez', 2000, 'hombre', 'yagoy90@gmail.com');
+
+    expect(estudiante_1.set_nombre('kevin')).to.be.equal('kevin');
+    expect(estudiante_1.set_apellido('alonso')).to.be.equal('alonso');
+    expect(estudiante_1.set_fecha_nacimiento(2000)).to.be.equal(2000);
+    expect(estudiante_1.set_fecha_nacimiento(2000)).not.to.be.equal(1999);
+    expect(estudiante_1.set_genero('hombre')).to.be.equal('hombre');
+    expect(estudiante_1.set_correo_electronico('yagoy90@hotmail.com')).to.be.equal('yagoy90@hotmail.com');
   });
 
 });
